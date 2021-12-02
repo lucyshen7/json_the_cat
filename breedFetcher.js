eb2d71d3b226e640f5fe196f7a07309aad4a4c6b
Lucy Shen <lucyshen77@gmail.com>
2021-12-02 22:39:11 +0000

    breedFetcher.js

---
+++ breedFetcher.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const breed = process.argv[2];
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`; // API endpoint
+
+request(url, (error, response, body) => {
+  if (!error) {
+    const data = JSON.parse(body);
+    if (breed !== data[0].name) {
+      console.log(`ERROR: Breed '${breed}' not found`);
+    } else { // breed is found
+      const description = data[0].description;
+      printResult(description);
+    }
+  } else {
+    console.log('The request failed. Error:', error); // print the error if one occurred
+  }
+});
+
+const printResult = (description) => {
+  console.log(description);
+};
